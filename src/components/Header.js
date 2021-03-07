47398a375fad9f908558e80fc8dd43175bd4c448
aliao23 <54867549+aliao23@users.noreply.github.com>
2021-03-07 03:37:27 -0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>Welcome to Project Connect.</h1>
-            <p>Bridging the age gap, one human at a time.</p>
+            <h1>Hello! </h1>
+            <h1>Welcome to <projecttitle>Project Connect</projecttitle>.</h1><br></br>
+            <p>Bridging the generation gap, one human at a time.</p><br></br>
             
         </div>
     )
