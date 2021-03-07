265c9edc1af44f2949eddcfae65c5084e1533d55
Ulam22 <ambros@simmons.edu>
2021-03-07 06:40:35 -0500

    home edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hello! </h1>
+        <div className="header">
             <h1>Welcome to <projecttitle>Project Connect</projecttitle>.</h1><br></br>
             <p>Bridging the generation gap, one human at a time.</p><br></br>
             
