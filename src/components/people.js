17aecc448be2f24f2896840fdd33d065e05fc8c3
Ulam22 <ambros@simmons.edu>
2021-03-07 08:35:42 -0500

    cards

--- src/components/people.js
+++ src/components/people.js
@@ -2,22 +2,23 @@ import React from "react";
 import { Card, Col } from "react-bootstrap";
 
 // Define the resource card object
-const  PeopleCard = ({ name, image, imageALT, text }) => (
+const PeopleCard = ({ name, link, image, imageALT, text }) => (
   <Col md="3">
-      <Card
-        className="people__rounded-card" style={{width:'18rem'}}
-      >
-        
-        <Card.Img
+    
+    <Card >
+     
+      <Card.Body className="people__rounded-card">
+      <a href={link} className="people__rounded-card-link">  <Card.Img
           variant="top"
           src={image}
           alt={imageALT}
           text={text}
-          className="people__rounded-card"
-        />
-        <label className="resources__card-label"> {name} </label>
-      </Card>
-
+          className="people__rounded-card-pfp"
+        /> 
+</a> 
+        <h4 className="people-card-label"> {name} </h4>
+      </Card.Body>{" "}
+  </Card>
   </Col>
 );
 
